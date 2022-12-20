e58f8a172b12fce91e1c8c04c93a593edd79b194
Migggzz <miggz.gommz@gmail.com>
2022-12-19 21:30:54 -0600

    otro commit para arreglar netlify

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,19 @@
+import { useGetItem } from "../hooks/useGetItem";
+import { ItemList } from "./ItemList";
+import { Loading } from "./Loading";
+
+const ItemListContainer = () => {
+  const items = useGetItem();
+
+  if (!items) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="h-full">
+      <ItemList products={items} />
+    </div>
+  );
+};
+
+export default ItemListContainer;
